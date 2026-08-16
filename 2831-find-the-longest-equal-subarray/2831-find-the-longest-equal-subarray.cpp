a2d7269c5a91d2f444e443c1b1781e3647cd780a
Mayank kumar <mayankkumarsharma244@gmail.com>
2026-08-16 11:06:57 +0530

    Time: 112 ms (86.14%), Space: 184.5 MB (71.13%) - LeetHub

---
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestEqualSubarray(vector<int>& nums, int k) {
+        size_t n = nums.size();
+        unordered_map<int, int> mp;
+        int i=0, j=0, maxf=0, res=0;
+        while(j<n){
+            mp[nums[j]]++;
+            maxf=max(maxf, mp[nums[j]]);
+            while((j-i+1)-maxf>k){
+                mp[nums[i]]--;
+                i++;
+            }
+            res=max(res, maxf);
+            j++;
+        }
+        return res;
+    }
+};
