dd5e5a3de6aec6394aa0f055a4bfc258dc8483c8
Mayank kumar <mayankkumarsharma244@gmail.com>
2026-09-06 17:34:45 +0530

    Time: 16 ms (30.23%), Space: 14.9 MB (38.15%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -6,7 +6,7 @@ public:
             mp[st]++;
         }
         for (int i = 0; i < s.size(); i++) {
-            if (mp[s[i]] == 1) {
+            if (mp[s[i]] < 2) {
                 return i;
             }
         }
